0723e8713eb5e079d5d5cc3e90cdc34f86a05ada
Timmy Wong <tmoiyael@gmail.com>
2020-12-18 21:57:35 -0800

    Add files via upload

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,9 +1,9 @@
-def counting_game(initial_numbers, target):
+def counting_game(starting_numbers, target):
     last_index = {}
-    for i, n in enumerate(initial_numbers[:-1]):
+    for i, n in enumerate(starting_numbers[:-1]):
         last_index[n] = i
-    last_num = initial_numbers[-1]
-    for i in range(len(initial_numbers) - 1, target - 1):
+    last_num = starting_numbers[-1]
+    for i in range(len(starting_numbers) - 1, target - 1):
         if last_num in last_index:
             new_num = i - last_index[last_num]
         else:
@@ -13,9 +13,9 @@ def counting_game(initial_numbers, target):
     return last_num
 
 def main():
-    initial_numbers = [11,18,0,20,1,7,16]
-    print(counting_game(initial_numbers, 2020))
-    print(counting_game(initial_numbers, 30000000))
+    starting_numbers = [11,18,0,20,1,7,16]
+    print(counting_game(starting_numbers, 2020))
+    print(counting_game(starting_numbers, 30000000))
 
 if __name__ == "__main__":
     main()
